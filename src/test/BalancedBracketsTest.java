13be1c77e7d0526f00242e79bcea14d29af8c38a
cfelix700 <cfelix700@gmail.com>
2023-01-26 20:10:01 -0600

    almost done

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,50 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsBalancedWithOtherChars(){
+        String input = "[Hello] World";
+        boolean expectedOutput = true;
+        assertEquals(expectedOutput, BalancedBrackets.hasBalancedBrackets((input)));
+    }
+    @Test
+    public void testHasBalancedBracketsEmptyString(){
+        String input = "";
+        boolean expectedOutput = true;
+        assertEquals(expectedOutput, BalancedBrackets.hasBalancedBrackets(input));
+    }
+    @Test
+    public void balancedBracketsMismatchedBrackets(){
+        String input = "[{]}";
+        boolean expectedOutput = false;
+        assertEquals(expectedOutput, BalancedBrackets.hasBalancedBrackets(input));
+    }
+
+    @Test
+    public void hasBalancedNestedBrackets(){
+        String input = "[[{()}]]";
+        boolean expectedOutput = true;
+        assertEquals(expectedOutput, BalancedBrackets.hasBalancedBrackets(input));
+    }
+
+    @Test
+    public void hasUnbalancedNestedBrackets(){
+        String input = "[[{(}]]";
+        boolean expectedOutput = false;
+        assertEquals(expectedOutput, BalancedBrackets.hasBalancedBrackets(input));
+    }
+    @Test
+    public void singleBracketsBeforeOtherCharacter(){
+        String spec = "a single set of balanced brackets before other characters return true";
+        String input = "[]LaunchCode";
+        boolean result = BalancedBrackets.hasBalancedBrackets(input);
     }
 
 
